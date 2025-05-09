3d84817a972f98b756363d4305654239dd7ba684
🖤 𝓐𝓛𝓨𝓧 - 天权🖤🐉 <179976644+alyxshang@users.noreply.github.com>
2025-05-10 00:58:05 +0200

    Update init.lua

--- lua/jadewisdom/init.lua
+++ lua/jadewisdom/init.lua
@@ -1,5 +1,17 @@
+-- JADEWISDOM.NVIM by Alyx Shang.
+-- Licensed under the FSL v1.
+
+-- Defining an empty
+-- table for the module.
 local M = {}
+
+-- Setting a default
+-- for the "transparent"
+-- setting.
 M.transparent = false
+
+-- Defining a setup closure
+-- for the module.
 M.setup = function(opts)
   if opts.transparent then
     M.transparent = true
@@ -7,4 +19,7 @@ M.setup = function(opts)
     M.transparent = false
   end
 end
+
+-- Exporting
+-- the module.
 return M
