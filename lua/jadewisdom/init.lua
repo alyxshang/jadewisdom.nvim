8312e28050d02a7eefa3618407d8888be7adc695
🖤 𝓐𝓛𝓨𝓧 - 天权🖤🐉 <179976644+alyxshang@users.noreply.github.com>
2025-05-10 00:30:49 +0200

    Update init.lua

--- lua/jadewisdom/init.lua
+++ lua/jadewisdom/init.lua
@@ -1,6 +1,10 @@
 local M = {}
+M.transparent = false
 M.setup = function(opts)
-  M.opts = opts
+  if opts.transparent then
+    M.transparent = true
+  else
+    M.transparent = false
+  end
 end
-
 return M
