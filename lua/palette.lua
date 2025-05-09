86145285eb8ee6a9fd7b9bf2c554d18206fa99ec
🖤 𝓐𝓛𝓨𝓧 - 天权🖤🐉 <179976644+alyxshang@users.noreply.github.com>
2025-05-10 00:56:13 +0200

    Update palette.lua

--- lua/palette.lua
+++ lua/palette.lua
@@ -4,8 +4,8 @@
 -- Defining the
 -- colorscheme's
 -- color palettes.
-function palettes(transparentSetting)
-  local themeDarkPalette = {
+function palette(transparentSetting)
+  local themePalette = {
     bg = "#453424",
     fg = "#BA925A",
     fg2 = "#AB8653",
@@ -27,32 +27,8 @@ function palettes(transparentSetting)
     transparent = transparentSetting
   }
 
-  -- The theme's light palette.
-  local themeLightPalette = {
-    bg = "#BA925A",
-    fg = "#453424",
-    fg2 = "#AB8653",
-    fg3 = "#9C7B4C",
-    fg4 = "#8D6F44",
-    bg2 = "#544436",
-    bg3 = "#635447",
-    bg4 = "#726559",
-    keywordColor = "#95847A",
-    builtinColor = "#F6E2BC",
-    constColor = "#F9F4EE",
-    commentColor = "#DDC4A0",
-    funcColor = "#E4B455",
-    strColor = "#F6E2BC",
-    typeColor = "#A58B7C",
-    varColor = "#D3B589",
-    warningColor = "#FFF7F2",
-    warning2Color = "#FFF7F2",
-    transparent = transparentSetting
-  }
-
   return {
-    dark = themeDarkPalette,
-    light = themeLightPalette
+    palette = themePalette
   }
 end
 
@@ -60,5 +36,5 @@ end
 -- for obtaining the 
 -- theme's color palettes.
 return {
-  palettes = palettes
+  palette = palette
 }
